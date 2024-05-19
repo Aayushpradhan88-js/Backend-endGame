8b7db39a030a07ce12c982e058bac8d8bc93765f
Aayush Pradhan <154524186+Aayushpradhan88-js@users.noreply.github.com>
2024-05-19 08:27:46 +0000

    connecting mongodb to server

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://127.0.1:27017/backenddb");
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render("index");
-});
+const userSchema = mongoose.Schema({
 
-module.exports = router;
+  username: String,
+  name: String,
+  age: Number
+
+})
+
+module.exports = mongoose.model("user", userSchema);
+
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+let userModel = require('./index');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/data', async function(req, res) {
+  const userRes = await userModel.create({
+    username: "Aayush",
+    name: "aayush",
+    age: 19
+  })
+
+  res.send(userRes);  
+})
 
 module.exports = router;
